13051e70bb2c0a5ea3662689b96ed50cf13e9973
A Guinane <alexguinane@gmail.com>
2017-06-17 08:15:58 +1000

    Add pelican stuff

---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# Default Settings
+AUTHOR = 'Alex Guinane'
+
+SITENAME = 'Alex Guinane'
+SITETITLE = SITENAME
+SITESUBTITLE = 'The blog formerly known as [Tales From Turgi]'
+SITELOGO = '/images/avatar.jpg'
+FAVICON = '/images/favicon.ico'
+TIMEZONE = 'Australia/Brisbane'
+DEFAULT_DATE_FORMAT = ('%d %b %Y')
+DEFAULT_LANG = 'en'
+
+# Output paths
+PATH = 'content'
+STATIC_PATHS = ['images']
+
+# Theme Settings
+THEME = 'themes/Flex'
+
+# Blogroll
+MAIN_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+MENUITEMS = ()
+LINKS = (('Blog', '/blog.html'),
+         ('Archives', '/archives.html'),
+         ('Categories', '/categories.html'),
+         ('Tags', '/tags.html'),
+         )
+INDEX_SAVE_AS = "blog.html"
+ARTICLE_URL = 'posts/{date:%Y}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+# Social widget
+GITHUB_URL = "https://github.com/aguinane"
+TWITTER_USERNAME = "alexguinane"
+SOCIAL = (
+    ("twitter", 'https://twitter.com/alexguinane'),
+    ("github", 'https://github.com/aguinane'),
+    ("linkedin", 'https://www.linkedin.com/in/alexguinane/'),
+)
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DEFAULT_PAGINATION = 10
