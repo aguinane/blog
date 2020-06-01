aa12f71b719a401a129a099e50a4840192a8e64b
Alex Guinman <alex@guinman.id.au>
2020-06-02 07:12:30 +1000

    Resize images

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import os
 import re
+from PIL import Image
 from pathlib import Path
 from invoke import task
 
@@ -23,6 +24,19 @@ def lowercase_jpg(c):
 @task
 def remove_metadata(c):
     c.run("exiftool -r -overwrite_original -P -all= content -ext jpg -ext jpeg")
+
+
+@task
+def resize_images(c, pre=[lowercase_jpg, remove_metadata]):
+    files = list(Path("content").rglob("*.jpg"))
+    for f in files:
+        filesize = f.stat().st_size
+        if filesize > 300000:
+            im = Image.open(f)
+            width, height = im.size
+            if width > 2024 or height > 2024:
+                cmd = f"mogrify -verbose -resize 2024\>x2024\> {str(f)}"
+                c.run(cmd)
 
 
 @task
